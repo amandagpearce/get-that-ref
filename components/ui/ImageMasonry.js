751eacb1e7bebb2bcb5f60dbf75fd0194d3473fa
Amanda Pearce <amandagpearce@gmail.com>
2023-09-07 19:06:13 -0300

    Image masonry update

--- components/ui/ImageMasonry.js
+++ components/ui/ImageMasonry.js
@@ -1,41 +1,31 @@
-import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Masonry from '@mui/lab/Masonry';
-import { styled } from '@mui/material/styles';
+import styles from './ImageMasonry.module.css'; // Import the CSS module
 
-const Label = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(0.5),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  borderBottomLeftRadius: 0,
-  borderBottomRightRadius: 0,
-}));
-
-export default function ImageMasonry({ itemData }) {
+const ImageMasonry = ({ itemData }) => {
   return (
     <Box>
-      <Masonry columns={7} spacing={2}>
+      <Masonry columns={6} spacing={3}>
         {itemData.map((item, index) => (
-          <div key={index} data-series-year={item.year}>
-            <div className="image-container">
-              <img
-                src={`${item.imageUrl}?w=690&auto=format`}
-                srcSet={`${item.imageUrl}?w=690&auto=format&dpr=2 2x`}
-                alt={item.title}
-                loading="lazy"
-                style={{
-                  display: 'block',
-                  width: '100%',
-                }}
-              />
-            </div>
+          <div
+            key={index}
+            data-series-year={item.year}
+            className={styles['image-container']}
+          >
+            <img
+              src={`${item.imageUrl}?w=690&auto=format`}
+              srcSet={`${item.imageUrl}?w=690&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+              className={styles['image']} // Apply the CSS class for the image
+            />
             <div className="refereces-container"></div>
           </div>
         ))}
       </Masonry>
     </Box>
   );
-}
+};
+
+export default ImageMasonry;
