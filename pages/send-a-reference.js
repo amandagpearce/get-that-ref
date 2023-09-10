0a52b158562bcffc532f9f0ebe636dfe843bfa71
Amanda Pearce <amandagpearce@gmail.com>
2023-09-10 10:17:26 -0300

    form validation

--- pages/send-a-reference.js
+++ pages/send-a-reference.js
@@ -20,14 +20,61 @@ const SendAReference = () => {
   const [episode, setEpisode] = useState('');
   const [season, setSeason] = useState('');
 
+  const [validity, setValidity] = useState({
+    title: false,
+    year: false,
+    episode: true,
+    season: true,
+    artwork: false,
+    artist: false,
+    sceneDescription: false,
+  });
+
+  const [touched, setTouched] = useState({
+    title: false,
+    year: false,
+    episode: true,
+    season: true,
+    artwork: false,
+    artist: false,
+    sceneDescription: false,
+  });
+
   const handleRadioChange = (event) => {
     setIsMovie(event.target.value === 'movie');
+  };
+
+  const handleInputChange = (field, value) => {
+    const isValid = !!value;
+
+    setTouched((prevState) => ({
+      ...prevState,
+      [field]: true,
+    }));
+
+    setValidity((prevState) => ({
+      ...prevState,
+      [field]: isValid,
+    }));
+  };
+
+  const handleBlur = (field) => {
+    if (!validity[field]) {
+      setTouched((prevState) => ({
+        ...prevState,
+        [field]: true,
+      }));
+    }
+  };
+
+  const isFormValid = () => {
+    return Object.values(validity).every((fieldValid) => fieldValid);
   };
 
   const handleFileChange = (event) => {
     // Handle file selection here, you can set the selected file to state
     const file = event.target.files[0];
-    setSelectedFile(file);
+    // setSelectedFile(file);
   };
 
   return (
@@ -43,7 +90,7 @@ const SendAReference = () => {
       }}
       variant="outlined"
     >
-      <Grid container spacing={2} p={4} sx={{ alignItems: 'flex-end' }}>
+      <Grid container spacing={2} p={4} sx={{ alignItems: 'flex-start' }}>
         <Grid item xs={12}>
           <Typography level="h4" component="h1">
             <b>Welcome!</b>
@@ -53,10 +100,10 @@ const SendAReference = () => {
           </Typography>
         </Grid>
 
-        <Grid item xs={12} md={6} lg={4}>
-          <FormControl>
-            <Grid container alignItems="center">
-              <Grid item xs={4} sx={{ marginBottom: 2 }}>
+        <Grid item xs={12}>
+          <Grid container alignItems="center">
+            <Grid item sx={{ marginRight: '20px' }}>
+              <FormControl>
                 <RadioGroup
                   name="isMovie"
                   value={isMovie ? 'movie' : 'series'}
@@ -70,9 +117,11 @@ const SendAReference = () => {
                 >
                   <Radio value="movie" sx={{ marginRight: '10px' }} /> Movie
                 </RadioGroup>
-              </Grid>
+              </FormControl>
+            </Grid>
 
-              <Grid item xs={4} sx={{ marginBottom: 2 }}>
+            <Grid item sx={{ marginRight: '20px' }}>
+              <FormControl>
                 <RadioGroup
                   name="isMovie"
                   value={isMovie ? 'movie' : 'series'}
@@ -86,45 +135,75 @@ const SendAReference = () => {
                 >
                   <Radio value="series" sx={{ marginRight: '10px' }} /> Series
                 </RadioGroup>
-              </Grid>
-
-              <Grid item xs={12}>
-                <FormControl>
-                  <FormLabel sx={{ fontSize: '1.1rem' }}>Title*</FormLabel>
-                  <Input
-                    name="title"
-                    type="text"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    required
-                  />
-                </FormControl>
-              </Grid>
+              </FormControl>
             </Grid>
-          </FormControl>
+          </Grid>
+        </Grid>
 
-          {isMovie ? (
-            <>
+        <Grid item xs={12} md={6} lg={4}>
+          <Grid container alignItems="center">
+            <Grid item xs={12}>
+              <FormControl>
+                <FormLabel sx={{ fontSize: '1.1rem' }}>Title*</FormLabel>
+                <Input
+                  name="title"
+                  type="text"
+                  value={title}
+                  onChange={(e) => {
+                    setTitle(e.target.value);
+                    handleInputChange('title', e.target.value);
+                  }}
+                  onBlur={() => handleBlur('title')}
+                  required
+                  sx={{
+                    borderColor:
+                      touched.title && !validity.title ? 'red' : '#cdd7e1',
+                  }}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
               <FormControl sx={{ marginTop: 2, marginBottom: 2 }}>
-                <FormLabel sx={{ fontSize: '1.1rem' }}>Year*</FormLabel>
+                <FormLabel sx={{ fontSize: '1.1rem' }}>
+                  {isMovie ? 'Movie' : 'Series '} Year*
+                </FormLabel>
                 <Input
                   name="year"
                   type="number"
                   value={year}
-                  onChange={(e) => setYear(e.target.value)}
+                  onChange={(e) => {
+                    setYear(e.target.value);
+                    handleInputChange('year', e.target.value);
+                  }}
+                  onBlur={() => handleBlur('year')}
                   required
+                  sx={{
+                    borderColor:
+                      touched.year && !validity.year ? 'red' : '#cdd7e1',
+                  }}
                 />
               </FormControl>
-            </>
-          ) : (
+            </Grid>
+          </Grid>
+
+          {!isMovie && (
             <>
-              <FormControl sx={{ marginTop: 2, marginBottom: 2 }}>
+              <FormControl sx={{ marginBottom: 2 }}>
                 <FormLabel sx={{ fontSize: '1.1rem' }}>Episode</FormLabel>
                 <Input
                   name="episode"
                   type="text"
                   value={episode}
-                  onChange={(e) => setEpisode(e.target.value)}
+                  onChange={(e) => {
+                    setEpisode(e.target.value);
+                    handleInputChange('episode', e.target.value);
+                  }}
+                  onBlur={() => handleBlur('episode')}
+                  sx={{
+                    borderColor:
+                      touched.episode && !validity.episode ? 'red' : '#cdd7e1',
+                  }}
                 />
               </FormControl>
               <FormControl sx={{ marginTop: 2, marginBottom: 2 }}>
@@ -133,7 +212,15 @@ const SendAReference = () => {
                   name="season"
                   type="text"
                   value={season}
-                  onChange={(e) => setSeason(e.target.value)}
+                  onChange={(e) => {
+                    setSeason(e.target.value);
+                    handleInputChange('season', e.target.value);
+                  }}
+                  onBlur={() => handleBlur('season')}
+                  sx={{
+                    borderColor:
+                      touched.season && !validity.season ? 'red' : '#cdd7e1',
+                  }}
                 />
               </FormControl>
             </>
@@ -141,7 +228,7 @@ const SendAReference = () => {
         </Grid>
 
         <Grid item xs={12} md={6} lg={4}>
-          <FormControl sx={{ marginTop: 2, marginBottom: 2 }}>
+          <FormControl sx={{ marginBottom: 2 }}>
             <FormLabel sx={{ fontSize: '1.1rem' }}>
               Artwork referenced*
             </FormLabel>
@@ -149,8 +236,16 @@ const SendAReference = () => {
               name="artwork"
               type="text"
               value={artwork}
-              onChange={(e) => setArtwork(e.target.value)}
+              onChange={(e) => {
+                setArtwork(e.target.value);
+                handleInputChange('artwork', e.target.value);
+              }}
+              onBlur={() => handleBlur('artwork')}
               required
+              sx={{
+                borderColor:
+                  touched.artwork && !validity.artwork ? 'red' : '#cdd7e1',
+              }}
             />
           </FormControl>
 
@@ -160,14 +255,22 @@ const SendAReference = () => {
               name="artist"
               type="text"
               value={artist}
-              onChange={(e) => setArtist(e.target.value)}
+              onChange={(e) => {
+                setArtist(e.target.value);
+                handleInputChange('artist', e.target.value);
+              }}
+              onBlur={() => handleBlur('artist')}
               required
+              sx={{
+                borderColor:
+                  touched.artist && !validity.artist ? 'red' : '#cdd7e1',
+              }}
             />
           </FormControl>
         </Grid>
 
         <Grid item xs={12} md={6} lg={4}>
-          <FormControl sx={{ marginTop: 2, marginBottom: 2 }}>
+          <FormControl sx={{ marginBottom: 2 }}>
             <FormLabel sx={{ fontSize: '1.1rem' }}>
               Scene description*
             </FormLabel>
@@ -175,8 +278,18 @@ const SendAReference = () => {
               name="sceneDescription"
               type="text"
               value={sceneDescription}
-              onChange={(e) => setSceneDescription(e.target.value)}
+              onChange={(e) => {
+                setSceneDescription(e.target.value);
+                handleInputChange('sceneDescription', e.target.value);
+              }}
+              onBlur={() => handleBlur('sceneDescription')}
               required
+              sx={{
+                borderColor:
+                  touched.sceneDescription && !validity.sceneDescription
+                    ? 'red'
+                    : '#cdd7e1',
+              }}
             />
           </FormControl>
 
@@ -187,7 +300,7 @@ const SendAReference = () => {
             <Input
               name="sceneImage"
               type="file"
-              accept=".png, .jpeg, .jpg" // Specify accepted file types
+              accept=".png, .jpeg, .jpg"
               onChange={handleFileChange}
               sx={{ paddingTop: '7px' }}
             />
@@ -198,9 +311,14 @@ const SendAReference = () => {
           <Button
             sx={{
               mt: 1,
+              width: '120px',
               background: 'linear-gradient(45deg, #ffe622, #ff54fd, #2196F3);',
               fontSize: '1.1rem',
+              opacity: '1',
+              filter: `grayscale(${isFormValid() ? 0 : 1})`, // Apply grayscale filter when disabled
+              color: isFormValid() ? 'white' : '#000', // Change color to black when disabled
             }}
+            disabled={!isFormValid()}
           >
             Submit
           </Button>
