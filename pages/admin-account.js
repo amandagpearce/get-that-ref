24bb89de9d408b06a8d4f3693b431b25d760f405
Amanda Pearce <amandagpearce@gmail.com>
2023-09-11 13:36:22 -0300

    edit mode on references to approve list

--- pages/admin-account.js
+++ pages/admin-account.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -7,6 +8,11 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Grid, Container } from '@mui/material';
 import { useQuery, gql } from '@apollo/client';
+import { useMutation } from '@apollo/client';
+import Sheet from '@mui/joy/Sheet';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
 
 const GET_REFERENCES_TO_APPROVE = gql`
   query {
@@ -29,29 +35,41 @@ const GET_REFERENCES_TO_APPROVE = gql`
   }
 `;
 
+const UPDATE_REFERENCE = gql`
+  mutation UpdateReference(
+    $id: ID!
+    $artworkDescription: String
+    $artworkYear: Int
+    $size: String
+    $currentLocation: String
+  ) {
+    updateReference(
+      id: $id
+      artworkDescription: $artworkDescription
+      artworkYear: $artworkYear
+      size: $size
+      currentLocation: $currentLocation
+    ) {
+      id
+      # Include other fields you need in the response
+    }
+  }
+`;
+
 const AdminAccount = () => {
-  // const DUMMY_TO_APPROVE_ARRAY = [
-  //   {
-  //     productionType: 'movie',
-  //     productionTitle: 'The Dreamers',
-  //     productionYear: '2003',
-  //     artworkReferenced: 'Venus de Milo',
-  //     artist: 'Alexandre de Antioquia',
-  //     sceneDescription:
-  //       "Just as the Venus de Milo represents classical beauty and grace, Isabelle's wearing of black gloves against a dark background creates a striking contrast that accentuates her hands and, by extension, her sensuality. In both instances, the visual elements are carefully composed to evoke a sense of elegance and allure. While the Venus de Milo is a classical representation of feminine beauty in art, Isabelle's scene adds a contemporary, provocative twist to the exploration of sensuality. The black gloves, much like the timeless allure of the Venus de Milo, symbolize the enduring fascination with the aesthetics of desire and the complexities of human sexuality, making it a thought-provoking and visually captivating moment in the film.",
-  //     image:
-  //       'https://static.boredpanda.com/blog/wp-content/uploads/2020/12/art-references-in-movies-35-5fc8d799c8fc7__700.jpg',
-  //   },
-  //   {
-  //     productionType: 'movie',
-  //     productionTitle: 'The Dreamers',
-  //     productionYear: '2003',
-  //     artworkReferenced: 'Venus de Milo',
-  //     artist: 'Alexandre de Antioquia',
-  //     sceneDescription:
-  //       "Just as the Venus de Milo represents classical beauty and grace, Isabelle's wearing of black gloves against a dark background creates a striking contrast that accentuates her hands and, by extension, her sensuality. In both instances, the visual elements are carefully composed to evoke a sense of elegance and allure. While the Venus de Milo is a classical representation of feminine beauty in art, Isabelle's scene adds a contemporary, provocative twist to the exploration of sensuality. The black gloves, much like the timeless allure of the Venus de Milo, symbolize the enduring fascination with the aesthetics of desire and the complexities of human sexuality, making it a thought-provoking and visually captivating moment in the film.",
-  //   },
-  // ];
+  const [artwork, setArtwork] = useState();
+  const [artist, setArtist] = useState();
+  const [sceneDescription, setSceneDescription] = useState();
+  const [title, setTitle] = useState();
+  const [year, setYear] = useState();
+  const [episode, setEpisode] = useState();
+  const [season, setSeason] = useState();
+  const [artworkDescription, setArtworkDescription] = useState();
+  const [artworkYear, setArtworkYear] = useState();
+  const [size, setArtworkSize] = useState();
+  const [currentLocation, setCurrentLocation] = useState();
+  const [editData, setEditData] = useState(null);
+  const [updateReference] = useMutation(UPDATE_REFERENCE);
 
   const { loading, error, data } = useQuery(GET_REFERENCES_TO_APPROVE, {
     fetchPolicy: 'cache-and-network',
@@ -60,11 +78,52 @@ const AdminAccount = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  const handleEditClick = (reference) => {
+    setArtwork(reference.artworkTitle);
+    setArtist(reference.artist);
+    setSceneDescription(reference.sceneDescription);
+    setTitle(reference.productionTitle);
+    setYear(reference.productionYear);
+
+    if (reference.productionType === 'series') {
+      setEpisode(reference.episode);
+      setSeason(reference.season);
+    }
+
+    if (editData === reference.id) {
+      // If the clicked card is already in edit mode, exit edit mode
+      setEditData(null);
+    } else {
+      // Set the clicked card in edit mode
+      setEditData(reference.id);
+    }
+  };
+
+  const handleEditFormSubmit = () => {
+    // Send the mutation to update the reference
+    updateReference({
+      variables: {
+        id: editData.id,
+        artworkDescription: editData.artworkDescription,
+        artworkYear: editData.artworkYear,
+        size: editData.size,
+        currentLocation: editData.currentLocation,
+      },
+      // Refetch the data after the update to refresh the UI
+      refetchQueries: [{ query: GET_REFERENCES_TO_APPROVE }],
+    });
+    // Clear the editData state
+    setEditData(null);
+  };
+
   const cards = data.references.map((card, key) => {
+    const editMode = editData === card.id;
+
     return (
       <Grid item xs={12}>
-        <Card xs={12} sx={{ display: 'flex' }}>
+        <Card xs={12} sx={{ display: 'flex', flexWrap: 'wrap' }}>
           <CardMedia
+            xs={4}
             component="img"
             alt={card.productionTitle}
             height="auto"
@@ -75,36 +134,278 @@ const AdminAccount = () => {
                 : 'https://placehold.co/500x450'
             }
           />
-          <CardContent sx={{ boxShadow: 'none' }}>
-            <Typography gutterBottom variant="h6" component="h6" px={6} py={1}>
-              {card.artworkTitle}
-            </Typography>
-            <Typography gutterBottom variant="h6" component="h6" px={6} py={1}>
-              <b>from </b>
-              {card.artist}
-            </Typography>
-            <Typography gutterBottom variant="h6" component="p" px={6} py={1}>
-              <b>in </b>
-              {card.productionTitle}&nbsp;({card.productionYear})
-            </Typography>
 
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              px={6}
-              sx={{ textAlign: 'justify' }}
-            >
-              {card.sceneDescription}
-            </Typography>
+          <CardContent xs={8} sx={{ boxShadow: 'none' }}>
+            <CardActions xs={12}>
+              <Button
+                onClick={() => handleEditClick(card)}
+                variant={editMode ? 'contained' : 'outlined'}
+                color="warning"
+                size="medium"
+              >
+                Edit
+              </Button>
+              <Button
+                variant="outlined"
+                color="success"
+                size="medium"
+                disabled={editMode ? 'disabled' : false}
+              >
+                Approve
+              </Button>
+              <Button
+                variant="outlined"
+                color="error"
+                size="medium"
+                disabled={editMode ? 'disabled' : false}
+              >
+                Delete
+              </Button>
+            </CardActions>
+
+            <div style={{ padding: '0 10px', marginTop: '10px' }}>
+              {!editMode && (
+                <>
+                  <Typography gutterBottom variant="body1" component="p">
+                    <b>Artwork Title: </b>
+                    {card.artworkTitle}
+                  </Typography>
+                  <Typography gutterBottom variant="body1" component="p">
+                    <b>Artist: </b>
+                    {card.artist}
+                  </Typography>
+                  <Typography gutterBottom variant="body1" component="p">
+                    <b>Referenced in: </b>
+                    {card.productionType} {card.productionTitle}&nbsp;(
+                    {card.productionYear})
+                  </Typography>
+
+                  <Typography variant="body1" sx={{ textAlign: 'justify' }}>
+                    <b>Scene Description: </b>
+
+                    {card.sceneDescription}
+                  </Typography>
+                </>
+              )}
+
+              {editMode && (
+                <Sheet
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    borderRadius: 'sm',
+                    boxShadow: 'none',
+                    padding: '10px',
+                    boxSizing: 'border-box',
+                  }}
+                >
+                  <form onSubmit={handleEditFormSubmit}>
+                    <Grid
+                      container
+                      spacing={2}
+                      sx={{ alignItems: 'flex-start' }}
+                    >
+                      <Grid item xs={12} md={6} lg={4}>
+                        <Grid container alignItems="center">
+                          <Grid item xs={12}>
+                            <FormControl>
+                              <FormLabel sx={{ fontSize: '1.1rem' }}>
+                                {card.productionType === 'movie'
+                                  ? 'Movie '
+                                  : 'Series '}
+                                Title
+                              </FormLabel>
+                              <Input
+                                name="title"
+                                type="text"
+                                value={title}
+                                required
+                                onChange={(e) => {
+                                  setTitle(e.target.value);
+                                }}
+                              />
+                            </FormControl>
+                          </Grid>
+
+                          <Grid item xs={12}>
+                            <FormControl sx={{ marginTop: 2, marginBottom: 2 }}>
+                              <FormLabel sx={{ fontSize: '1.1rem' }}>
+                                {card.productionType === 'movie'
+                                  ? 'Movie '
+                                  : 'Series '}{' '}
+                                Year
+                              </FormLabel>
+                              <Input
+                                name="year"
+                                type="number"
+                                value={year}
+                                onChange={(e) => {
+                                  setYear(e.target.value);
+                                }}
+                              />
+                            </FormControl>
+                          </Grid>
+                        </Grid>
+
+                        {card.productionType === 'series' && (
+                          <>
+                            <FormControl sx={{ marginBottom: 2 }}>
+                              <FormLabel sx={{ fontSize: '1.1rem' }}>
+                                Episode
+                              </FormLabel>
+                              <Input
+                                name="episode"
+                                type="text"
+                                value={episode}
+                                onChange={(e) => {
+                                  setEpisode(e.target.value);
+                                }}
+                              />
+                            </FormControl>
+                            <FormControl sx={{ marginTop: 2, marginBottom: 2 }}>
+                              <FormLabel sx={{ fontSize: '1.1rem' }}>
+                                Season
+                              </FormLabel>
+                              <Input
+                                name="season"
+                                type="text"
+                                value={season}
+                                onChange={(e) => {
+                                  setSeason(e.target.value);
+                                }}
+                              />
+                            </FormControl>
+                          </>
+                        )}
+                      </Grid>
+
+                      <Grid item xs={12} md={6} lg={4}>
+                        <FormControl sx={{ marginBottom: 2 }}>
+                          <FormLabel sx={{ fontSize: '1.1rem' }}>
+                            Artwork Title
+                          </FormLabel>
+                          <Input
+                            name="artwork"
+                            type="text"
+                            value={artwork}
+                            onChange={(e) => {
+                              setArtwork(e.target.value);
+                            }}
+                          />
+                        </FormControl>
+
+                        <FormControl sx={{ marginTop: 2, marginBottom: 2 }}>
+                          <FormLabel sx={{ fontSize: '1.1rem' }}>
+                            Artist
+                          </FormLabel>
+                          <Input
+                            name="artist"
+                            type="text"
+                            value={artist}
+                            onChange={(e) => {
+                              setArtist(e.target.value);
+                            }}
+                          />
+                        </FormControl>
+
+                        <FormControl sx={{ marginBottom: 2 }}>
+                          <FormLabel sx={{ fontSize: '1.1rem' }}>
+                            Artwork year
+                          </FormLabel>
+                          <Input
+                            name="artworkYear"
+                            type="number"
+                            value={artworkYear}
+                            onChange={(e) => {
+                              setArtworkYear(e.target.value);
+                            }}
+                          />
+                        </FormControl>
+
+                        <FormControl sx={{ marginBottom: 2 }}>
+                          <FormLabel sx={{ fontSize: '1.1rem' }}>
+                            Artwork size
+                          </FormLabel>
+                          <Input
+                            name="artworkSize"
+                            type="text"
+                            value={size}
+                            onChange={(e) => {
+                              setArtworkSize(e.target.value);
+                            }}
+                          />
+                        </FormControl>
+                      </Grid>
+
+                      <Grid item xs={12} md={6} lg={4}>
+                        <FormControl sx={{ marginBottom: 2 }}>
+                          <FormLabel sx={{ fontSize: '1.1rem' }}>
+                            Scene description
+                          </FormLabel>
+                          <Input
+                            name="sceneDescription"
+                            type="text"
+                            value={sceneDescription}
+                            onChange={(e) => {
+                              setSceneDescription(e.target.value);
+                            }}
+                          />
+                        </FormControl>
+
+                        <FormControl sx={{ marginBottom: 2 }}>
+                          <FormLabel sx={{ fontSize: '1.1rem' }}>
+                            Artwork description
+                          </FormLabel>
+                          <Input
+                            name="artworkDescription"
+                            type="text"
+                            value={artworkDescription}
+                            onChange={(e) => {
+                              setArtworkDescription(e.target.value);
+                            }}
+                          />
+                        </FormControl>
+
+                        <FormControl sx={{ marginBottom: 2 }}>
+                          <FormLabel sx={{ fontSize: '1.1rem' }}>
+                            Artwork current location
+                          </FormLabel>
+                          <Input
+                            name="currentLocation"
+                            type="text"
+                            value={currentLocation}
+                            onChange={(e) => {
+                              setCurrentLocation(e.target.value);
+                            }}
+                          />
+                        </FormControl>
+                      </Grid>
+                      <Grid
+                        item
+                        xs={12}
+                        sx={{ display: 'flex', justifyContent: 'flex-end' }}
+                      >
+                        <Button
+                          type="submit"
+                          sx={{
+                            mt: 1,
+                            width: '120px',
+                            color: '#FFF',
+                            background:
+                              'linear-gradient(45deg, #ffe622, #ff54fd, #2196F3);',
+                            fontSize: '1.1rem',
+                            opacity: '1',
+                          }}
+                        >
+                          Submit
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </Sheet>
+              )}
+            </div>
           </CardContent>
-          <CardActions>
-            <Button variant="outlined" color="success" size="medium">
-              Approve
-            </Button>
-            <Button variant="outlined" color="error" size="medium">
-              Delete
-            </Button>
-          </CardActions>
         </Card>
       </Grid>
     );
